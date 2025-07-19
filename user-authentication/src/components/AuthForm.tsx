7061b16ca794d8c6f77c47963e888829d3f18a46
Anju Adhikari <anjuadhkari667@gmail.com>
2025-07-19 22:33:14 +0545

    added Authencation

---
+++ user-authentication/src/components/AuthForm.tsx
@@ -0,0 +1,16 @@
+
+
+type Props ={
+    type: 'login' | 'signup';
+}
+
+export default function AuthForm() {
+    const AuthForm = ({type:Props})=>{
+
+    }
+  return (
+    <div>
+
+    </div>
+  )
+}
