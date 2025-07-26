65aa701402ce19124448edc30f11ef07e3fd8b12
Anju Adhikari <anjuadhkari667@gmail.com>
2025-07-26 22:11:48 +0545

    basic zod

--- user-authentication/src/model/user.tsx
+++ user-authentication/src/model/user.tsx
@@ -1,60 +1,34 @@
-import mongoose, { Schema, Document } from "mongoose";
-export interface Message extends Document {
-  content: string;
-  createdAt: Date;
-}
+import React from 'react'
+import {z} from 'zod'
 
-const MessageSchema: Schema<Message> = new Schema({
-  content: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
+const userSchema = z.object({
+    firstName:z.string().optional(),
+    email:z.string().email(),
+    profileUrl:z.string().url(),
+    age:z.number().min(0, "Age must be a positive number").max(120, "Age must be less than or equal to 120"),
+    friends:z.array(z.string()).max(3),
+    isActive:z.boolean()
 
-  },
-});
+})
 
-export interface User extends Document{
-    username:string;
-    email:string;
-    password:string;
-    verifyCode:string;
-    verifyCodeExpires:Date;
-    isAcceptingMessages:boolean;
-    messages: Message[];
+const users = {
+    firstName :"Anju",
+    email:"anju@gmail.com",
+    profileUrl:"https://www.example.com",
+    age:18,
+    friends:["John", "Doe", "Jane"],
+    isActive:false
 
 }
+console.log(userSchema.safeParse(users))
+function user() {
+  return (
+    <div>
+    <h1 className='text-2xl font-bold'>User Validation</h1>
 
-const UserSchema: Schema<User> = new Schema({
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  verifyCode: {
-    type: String,
-    required: true,
-  },
-  verifyCodeExpires: {
-    type: Date,
-    required: true,
-  },
-  isAcceptingMessages: {
-    type: Boolean,
-    default: false,
-  },
-  messages: [MessageSchema],
-});
+
+    </div>
+  )
+}
+
+export default user
